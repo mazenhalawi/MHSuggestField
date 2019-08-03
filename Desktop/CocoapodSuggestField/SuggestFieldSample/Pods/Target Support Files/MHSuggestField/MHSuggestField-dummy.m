69193a6bfcd1d5dc06ce8c817dee51ecd8b92682
Mazen <mazen.halawi79@hotmail.com>
2019-08-03 10:00:38 +0300

    Added Sample Project

---
+++ Desktop/CocoapodSuggestField/SuggestFieldSample/Pods/Target Support Files/MHSuggestField/MHSuggestField-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MHSuggestField : NSObject
+@end
+@implementation PodsDummy_MHSuggestField
+@end
---
+++ Desktop/CocoapodSuggestField/SuggestFieldSample/SuggestFieldSample/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  SuggestFieldSample
+//
+//  Created by Mazen on 08/02/2019.
+//  Copyright © 2019 Mazen. All rights reserved.
+//
+
+import UIKit
+import MHSuggestField
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtSuggest:SuggestField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissMe)))
+        
+        txtSuggest.allowEmptyField = true
+        txtSuggest.canFilterList = true
+        txtSuggest.restrictValues = true
+        txtSuggest.datasource = ["Apple", "Orange", "Banana", "Tomato"]
+        txtSuggest.suggestDelegate = self
+    }
+
+    @objc private func dismissMe() {
+        self.view.endEditing(true)
+    }
+
+}
+
+extension ViewController : SuggestFieldDelegate {
+    
+    func alertNotInList(value: String) {
+        print("input value not in list")
+    }
+    
+    func suggestFieldDidBeginEditing(_ suggestField: SuggestField) {
+        print("did begin editing")
+    }
+    
+    func suggestFieldDidEndEditing(_ suggestField: SuggestField) {
+        print("did end editing")
+    }
+    
+    
+    
+}
